64bbbb85fc48d1a0de8eb6ed906cac861cdd7ef8
AtlasMaroc <moha.castellajo2002@gmail.com>
2024-05-08 05:05:54 +0100

    Added a flag option to make the script more dynamic and flexible

--- pipeline.sh
+++ pipeline.sh
@@ -1,26 +1,64 @@
 #!/bin/bash
 
 #defining parameters
-
-short_read="$1"
-long_read="$2"
+type=                             
+short_read=
+long_read= 
 
 if [[ $# -eq 0 ]] ; then echo "Enter the required argument"; exit 1; fi
+ 
 
-if [[ -z "$1" ]] ; then echo "you must provide short reads filename"; exit 1; fi
-if [[ -z "$2" ]] ; then echo "you must provide long reads filename"; exit 1; fi
+#a help menu showing how to use the script and what the expected arguments are.
+usage(){
+cat << EOF
+${0} -s short_reads.fastq -l long_reads.fastq -t paired|single 
+ please specify:
+ --> short reads path with the flag -s 
+ --> long reads path with the flag -l
+ --> type of the short reads if they are single or paired end with the flag -t
 
-if [[ ! -f "$1"  ]] ; then echo "$1" "does not exist"; exit 1; fi
-if [[ ! -f "$2" ]] ; then echo "$2" "does not exist"; exit 1; fi
+EOF
+}
+#FastQC qualtiy control:
+short_qc(){
+fastqc "$1"
+}
 
-if [[ ! "$1" == +(*.fastq|*.fastq.gz) ]] ; then echo "$1 is not a fastq file" ; exit 1; fi
-if [[ ! "$2" == +(*.fastq|*.fastq.gz) ]] ; then echo "$2 is not a fastq file" ; exit 1; fi 
+
+#Count all the args passed into the script and continue looping while we have them:
+while [[ $# -gt 0 ]]
+do
+	case "$1" in 
+           -h|--help|help)
+		   usage
+                
+		exit 1
+	        ;;
+	   -t)
+		#set the value of of type so we can use later in the script
+		type="${2:-}"
+		#check if -t flag has been given a value or not
+	        [[ -z "$type" ]] && printf "%s must have a value\n\n" "$1" 1>&2 && usage 1>&2 && exit 1
+		#check if -t flag have been given the correct argument
+	        [[ ! $type == (single|paired) ]] && echo '-t flag accept only two argument: single or paired' && exit 1
+                
+		shift 2
+		;;
+	   -s)
+                #set the value to 
+		short_reads="${2:-}"
+	        #check if -s flag has been given a value or not
+		[[ -z $short_reads ]] && printf "%s -s must have a value and be a viable file path\n\n" 1>&2 && exit 1
+		#check if -s has been given a viable path to fastq files
+		[[ ! -f $short_reads || $short_reads !== (*.fastq|*.fastq.gz) ]] && printf  \                                           "%s must be a fastq file\n\n" && exit 1	 
+
+		shift 2
+	   -l)
+	     #
+
+
+
 
 #FastQC Quality Control:
 
-check_qualtiy() {
-	fastqc "$short_read"
-}
-
-chek_qualtiy
 
