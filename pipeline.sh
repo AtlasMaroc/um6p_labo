ca07115c38bfdb64ec5a233b06880acaefa4bd12
AtlasMaroc <moha.castellajo2002@gmail.com>
2024-05-10 12:51:46 +0100

    added trimmomatic for trimming low quality bases and adapters, also made the script more verstile by checking for the presence of short paired or single end reads

--- pipeline.sh
+++ pipeline.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 #defining parameters
-type=                             
-short_read=
-long_read= 
+type=
+short_read_se=
+long_read=
+short_read_forward=
+short_read_reverse=
 
-if [[ $# -eq 0 ]] ; then echo "Enter the required argument"; exit 1; fi
+if [[ $# -eq 6 ]] ; then echo "Enter the required argument"; exit 1; fi
  
 
 #a help menu showing how to use the script and what the expected arguments are.
 usage(){
 cat << EOF
-${0} -s short_reads.fastq -l long_reads.fastq -t paired|single 
+${0} -se short_single_end.fastq -l long_reads.fastq -pe forward_short.fastq reverse_short.fastq 
  please specify:
- --> short reads path with the flag -s 
+ --> single end short reads with the flag -se 
+ --> paired end short reads with the flag -pe s
  --> long reads path with the flag -l
  --> type of the short reads if they are single or paired end with the flag -t
 
 EOF
-}
-#FastQC qualtiy control:
-short_qc(){
-fastqc "$1"
 }
 
 
@@ -33,32 +32,82 @@ do
 		   usage
                 
 		exit 1
-	        ;;
-	   -t)
+       	        ;;
+	   -pe)
 		#set the value of of type so we can use later in the script
-		type="${2:-}"
+		type=pe
+		short_reads_forward="${2:-}"
+		short_reads_reverse="${3:-}"
 		#check if -t flag has been given a value or not
-	        [[ -z "$type" ]] && printf "%s must have a value\n\n" "$1" 1>&2 && usage 1>&2 && exit 1
+		([[ -z "$short_reads_forward" ]] || [[ -z "$short_reads_reverse" ]]) &&          \                                        printf "%s must have a value\n\n" "$1" 1>&2 && usage 1>&2 && exit 1
 		#check if -t flag have been given the correct argument
-	        [[ ! $type == (single|paired) ]] && echo '-t flag accept only two argument: single or paired' && exit 1
+	        [[ ! -f $short_reads_forward || $short_reads_forward !== (*.fastq|*.fastq.gz) ]] &&  \                                    echo 'must be a fastq file' && exit 1
                 
-		shift 2
+		shift 3
 		;;
-	   -s)
-                #set the value to 
-		short_reads="${2:-}"
+	   -se)
+                #set the value to
+	        type=se	
+		short_reads_se="${2:-}"
 	        #check if -s flag has been given a value or not
-		[[ -z $short_reads ]] && printf "%s -s must have a value and be a viable file path\n\n" 1>&2 && exit 1
+		[[ -z $short_reads_se ]] && printf "%s -s must have a value and be a viable file path\n\n" 1>&2 && exit 
 		#check if -s has been given a viable path to fastq files
-		[[ ! -f $short_reads || $short_reads !== (*.fastq|*.fastq.gz) ]] && printf  \                                           "%s must be a fastq file\n\n" && exit 1	 
+		[[ ! -f $short_reads_se || $short_reads_se !== (*.fastq|*.fastq.gz) ]] && printf  \                                           "%s must be a fastq file\n\n" && exit 1	 
 
 		shift 2
+
+		;;
 	   -l)
-	     #
+	     #set the value to
+	     long_reads="${2:-}"
+	     #check if the -t has been given a value or not
+	     [[ -z $long_reads ]] && printf "%s -l must have a value and be a viable path\n\n" >&2 && exit 1
+	     #check if -l has been given a viable path to fastq files
+	     [[ ! -f $long_reads || $long_reads !== (*.fastq|*.fastq.gz) ]] && printf \
+		     "%s must be a fastq file\n\n" && exit 1
+	     
+	     shift 2
 
+	        ;;
+	    *)
+	     printf "%s enter a valid option\n\n" >&2 && usage >&2 && exit 1
+	       
+	        ;;
 
+       esac 
 
+done
 
-#FastQC Quality Control:
+#generating a report of base quality using FastQC:
+quality_control(){
 
+	fastqc "$short_reads_se" "$short_reads_forward" "$short_reads_reverse"
+}
+
+quality_control
+
+#trimming low quality bases using Trimmomatic
+
+#trimming pairend short reads:
+
+trimming_pe(){
+	java -jar trimmomatic-0.39.jar PE "$short_reads_forward" "$short_read_reverse" paired_${short_read_forward} unpaired_${short_read_forward} paired_${short_read_reverse} unpaired_${short_read_reverse} ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:True LEADING:3 TRAILING:3 MINLEN:50
+
+}
+
+#trimming single end reads:
+
+trimming_se(){
+         java -jar trimmomatic-0.35.jar SE -phred33 "$short_reads_se" output_${short_reads_se} ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
+
+}
+
+#checking the type of short reads 
+
+if [[ $type == paired ]]
+do
+	trimming_pe
+else
+	trimming_se
+done
 
